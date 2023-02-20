3fecb679b3cbfe71535ce1d78e39000c4395e07b
Lautaro Rodriguez Xavier <lrodriguezxavier@yahoo.com>
2023-02-20 16:01:59 -0300

    array names modified

--- 2-d-arrays.js
+++ 2-d-arrays.js
@@ -29,8 +29,8 @@ function twoDimensionalProduct(array) {
   return product;
 }
 
-let arr1 = [[6, 4], [5], [3, 1]];
-console.log(twoDimensionalProduct(arr1)); // 360
+let arr3 = [[6, 4], [5], [3, 1]];
+console.log(twoDimensionalProduct(arr3)); // 360
 
-let arr2 = [[11, 4], [2]];
-console.log(twoDimensionalProduct(arr2)); // 88
+let arr4 = [[11, 4], [2]];
+console.log(twoDimensionalProduct(arr4)); // 88
