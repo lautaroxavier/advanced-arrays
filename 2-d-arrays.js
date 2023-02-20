40342326075be9f0f70584988d2b8165c0efedd9
Lautaro Rodriguez Xavier <lrodriguezxavier@yahoo.com>
2023-02-20 16:07:13 -0300

    max in matrix

--- 2-d-arrays.js
+++ 2-d-arrays.js
@@ -34,3 +34,25 @@ console.log(twoDimensionalProduct(arr3)); // 360
 
 let arr4 = [[11, 4], [2]];
 console.log(twoDimensionalProduct(arr4)); // 88
+
+// MAX IN MATRIX
+
+function maxInMatrix(matrix) {
+  let max;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] > max || max === undefined) {
+        max = matrix[i][j];
+      }
+    }
+  }
+  return max;
+}
+
+matrix = [
+  [11, 2, -99],
+  [20, 19, 10],
+  [47, 72, 56],
+];
+
+console.log(maxInMatrix(matrix)); // 72
